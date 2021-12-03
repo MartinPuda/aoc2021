f4ed716ed38477c5ae07eff5d42df658852d421c
MartinPuda <mapuda@seznam.cz>
2021-12-03 09:32:35 +0100

    Day 03

---
+++ src/aoc2021/day03.clj
@@ -0,0 +1,38 @@
+(ns aoc2021.day03
+  (:require [clojure.string :as s])
+  (:gen-class))
+
+(defn get-input []
+  (->> (slurp "resources/public/input03.txt")
+       (s/split-lines)))
+
+(defn gamma-rate [data]
+  (map #(first (second (sort-by val (frequencies %)))) data))
+
+(defn epsilon-rate [data]
+  (map #(ffirst (sort-by val (frequencies %))) data))
+
+(defn vector->binary [v]
+  (Integer/parseInt (s/join "" v) 2))
+
+(defn part1 [data]
+  (let [columns (apply map vector data)]
+    (->> columns
+         ((juxt gamma-rate epsilon-rate))
+         (map vector->binary)
+         (apply *))))
+
+(defn rating [position data type]
+  (let [columns (apply map vector data)]
+    (if (= (count data) 1) (first data)
+      (let [f (frequencies (nth columns position))
+            most-common (if (= (f \0) (f \1))
+                          (if (= type :oxygen) \1 \0)
+                          (ffirst (sort-by val (if (= type :oxygen) > <) f)))]
+        (recur (inc position)
+               (filter #(= (nth % position) most-common) data)
+               type)))))
+
+(defn part2 [data]
+  (* (vector->binary (rating 0 data :oxygen))
+     (vector->binary (rating 0 data :CO2))))
--- test/aoc2021/core_test.clj
+++ test/aoc2021/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [aoc2021.core :refer :all]
             [aoc2021.day01 :as day01]
-            [aoc2021.day02 :as day02]))
+            [aoc2021.day02 :as day02]
+            [aoc2021.day03 :as day03]))
 
 (deftest day01
   (testing "Part 1"
@@ -15,3 +16,9 @@
     (is (= (day02/part1 (day02/get-input)) 2272262)))
   (testing "Part2"
     (is (= (day02/part2 (day02/get-input)) 2134882034))))
+
+(deftest day03
+  (testing "Part1"
+    (is (= (day03/part1 (day03/get-input)) 3969000)))
+  (testing "Part2"
+    (is (= (day03/part2 (day03/get-input)) 4267809))))
